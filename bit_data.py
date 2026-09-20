35c771befeb40a9d58836dc5309e3efa986ae2b0
Mohd Ali <mohdalii1923@gmail.com>
2026-09-20 23:15:15 +0530

    Refactor student information form with Streamlit

--- bit_data.py
+++ bit_data.py
@@ -83,6 +83,7 @@ if st.session_state.admin_clicked:
             st.session_state.admin_verified=True
 if(st.session_state.admin_verified):
     st.subheader("All students records:")
+cursor=connection.cursor(dictionary=True)
     sql="""
     SELECT * FROM btech_3rd_year;
     """
