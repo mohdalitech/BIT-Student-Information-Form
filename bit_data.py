5b27391558eccbe2479e00bd0dcd05bcc3ee237f
Mohd Ali <mohdalii1923@gmail.com>
2026-09-20 22:20:54 +0530

    Add admin verification and record management features

--- bit_data.py
+++ bit_data.py
@@ -16,8 +16,41 @@ col1,col2=st.columns(2)
 with col1:
     st.header("🎓STUDENT INFORMATION FORM")
 with col2:
-    if(st.button("Admin Page",type="primary")):
-        st.switch_page("pages/admin.py")
+    if(st.button("Admin Portal",type="primary")):
+        if "admin_verified" not in st.session_state:
+        st.session_state.admin_verified=False
+    admin_name=st.text_input("Enter the admin username : ")
+    admin_pass=st.text_input("Enter the admin password : ")
+    if(st.button("Submit",type="primary")):
+        if(admin_name==st.secrets["admin_username"] and admin_pass==st.secrets["admin_password"]):
+            st.success("Your admin details are verified successfully...!!")
+            st.session_state.admin_verified=True
+        if(st.session_state.admin_verified):
+            st.subheader("All students records:")
+            sql="""
+            SELECT * FROM btech_3rd_year;
+            """
+            cursor.execute(sql)
+            results=cursor.fetchall()
+            if results:
+                st.dataframe(results,use_container_width=True)
+            else:
+                st.error("Not enough records found!")
+            if(st.button("Clear All Records",type="primary")):
+                confirm_check=st.selectbox(
+                    "Do you want to clear record? This action cannot be undone!",
+                    ["Yes","No"]
+                )
+                if(confirm_check=="Yes"):
+                    sql="""
+                    TRUNCATE TABLE btech_3rd_year
+                    """
+                    cursor.execute(sql)
+                    connection.commit()
+                else:
+                    st.success("The data has not been erased!!")
+        else:
+            st.error("Try entering correct credentials if you're are valid admin!")
 st.divider()
 student_id=st.text_input("Enter your student id :",placeholder="BIT_001")
 Student_Name=st.text_input("Enter your name :",placeholder="Rajit")
