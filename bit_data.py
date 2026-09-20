85b6df756ed120d8e75c7269b953ba833ccfb962
Mohd Ali <mohdalii1923@gmail.com>
2026-09-20 20:11:18 +0530

    Refactor student information form code

--- bit_data.py
+++ bit_data.py
@@ -15,6 +15,7 @@ st.set_page_config(
 )
 admin_page = st.Page("pages/admin.py",title="Admin Page")
 pg=st.navigation([admin page],position="hidden")
+pg.run()
 col1,col2=st.columns(2)
 with col1:
     st.header("🎓STUDENT INFORMATION FORM")
