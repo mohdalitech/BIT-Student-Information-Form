fa05df5e14ba6a93cdcdd30fd5efa1d5f5f0ac27
Mohd Ali <mohdalii1923@gmail.com>
2026-09-20 22:38:00 +0530

    Refactor student information form and admin portal

--- bit_data.py
+++ bit_data.py
@@ -68,39 +68,42 @@ if st.button("Submit Your Response",type="primary"):
                 st.error("No records found!!")
         except Exception as e:
             st.error(f"Error fetching record:{e}")
-
+if "admin_clicked" not in st.session_state:
+        st.session_state.admin_clicked=False
+if "admin_verified" not in st.session_state:
+        st.session_state.admin_verified=False
 if(st.button("Admin Portal",type="primary")):
-        if "admin_verified" not in st.session_state:
-            st.session_state.admin_verified=False
-            admin_name=st.text_input("Enter the admin username : ")
-            admin_pass=st.text_input("Enter the admin password : ")
-            if(st.button("Submit",type="primary")):
-                if(admin_name==st.secrets["admin_username"] and admin_pass==st.secrets["admin_password"]):
-                    st.success("Your admin details are verified successfully...!!")
-                    st.session_state.admin_verified=True
-        if(st.session_state.admin_verified):
-            st.subheader("All students records:")
+        st.session_state.admin_clicked=True
+if st.session_state.admin_clicked:
+    admin_name=st.text_input("Enter the admin username : ")
+    admin_pass=st.text_input("Enter the admin password : ")
+    if(st.button("Submit",type="primary")):
+        if(admin_name==st.secrets["admin_username"] and admin_pass==st.secrets["admin_password"]):
+            st.success("Your admin details are verified successfully...!!")
+            st.session_state.admin_verified=True
+if(st.session_state.admin_verified):
+    st.subheader("All students records:")
+    sql="""
+    SELECT * FROM btech_3rd_year;
+    """
+    cursor.execute(sql)
+    results=cursor.fetchall()
+    if results:
+        st.dataframe(results,use_container_width=True)
+    else:
+        st.error("Not enough records found!")
+    if(st.button("Clear All Records",type="primary")):
+        confirm_check=st.selectbox(
+            "Do you want to clear record? This action cannot be undone!",
+            ["Yes","No"]
+        )
+        if(confirm_check=="Yes"):
             sql="""
-            SELECT * FROM btech_3rd_year;
+            TRUNCATE TABLE btech_3rd_year
             """
             cursor.execute(sql)
-            results=cursor.fetchall()
-            if results:
-                st.dataframe(results,use_container_width=True)
-            else:
-                st.error("Not enough records found!")
-            if(st.button("Clear All Records",type="primary")):
-                confirm_check=st.selectbox(
-                    "Do you want to clear record? This action cannot be undone!",
-                    ["Yes","No"]
-                )
-                if(confirm_check=="Yes"):
-                    sql="""
-                    TRUNCATE TABLE btech_3rd_year
-                    """
-                    cursor.execute(sql)
-                    connection.commit()
-                else:
-                    st.success("The data has not been erased!!")
+            connection.commit()
         else:
-            st.error("Try entering correct credentials if you're are valid admin!")
+            st.success("The data has not been erased!!")
+else:
+    st.error("Try entering correct credentials if you're are valid admin!")
