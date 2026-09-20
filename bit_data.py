6be3e245fa3a21f34ec005acd6a7a311cefc8058
Mohd Ali <mohdalii1923@gmail.com>
2026-09-20 14:31:34 +0530

    Add files via upload

---
+++ bit_data.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import mysql.connector
+import json
+connection = mysql.connector.connect(
+    host=st.secrets["mysql"]["host"],
+    port=int(st.secrets["mysql"]["port"]),
+    user=st.secrets["mysql"]["user"],
+    password=st.secrets["mysql"]["password"],
+    database=st.secrets["mysql"]["database"]
+)
+cursor=connection.cursor()
+st.header("🎓BIT STUDENT INFORMATION FORM")
+st.divider()
+Student_ID=st.text_input("Enter your student id :",placeholder="BIT_001")
+Student_Name=st.text_input("Enter your name :",placeholder="Rajit")
+Gender=st.selectbox(
+    "Choose your gender :",
+    ["M","F"]
+)
+cgpa=st.number_input("Enter your current cgpa : ",placeholder="8.12")
+Attendance_Percentage=st.number_input("What is percentage of your attendance ?",placeholder="75.5")
+Total_distance=st.number_input("Enter total distance from home to BIT ?",placeholder="20.00")
+Roll_No=st.text_input("Enter your 13 digit roll no :",placeholder="1234567890012")
+Address=st.text_area("What is the your address ?",placeholder="Complete address")
+dob=st.date_input("Enter your date of birth : ")
+Class_Time=st.time_input("At what time class starts ? ")
+Admission_DateTime=st.datetime_input("When have you taken admission ? ")
+Skills=st.text_input("Enter your 3 skills comma separated : ",placeholder="python,java,writing")
+Semester_Status=st.selectbox(
+    "What is your semester status ? ",
+    ["Active","Completed","Dropped"]
+)
+if st.button("Submit Your Response",type="primary"):
+    skills=[skill.strip() for skill in Skills.split(",")]
+    if(len(skills)!=3):
+        st.toast("Skills count is not 3!")
+    sql="""
+    INSERT INTO btech_3rd_year
+    (Student_ID,Student_Name,Gender,cgpa,Attendance_Percentage,Total_distance,Roll_No,Address,dob,Class_Time,Admission_DateTime,Skills,Semester_Status)
+    VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+    """
+    cursor.execute(sql,(Student_ID,Student_Name,Gender,cgpa,Attendance_Percentage,Total_distance,Roll_No,Address,dob,Class_Time,Admission_DateTime,json.dumps(skills),Semester_Status))
+    connection.commit()
+    st.success("Your response has been recorded..!")
