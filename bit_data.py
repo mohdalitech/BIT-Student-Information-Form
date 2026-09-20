4e55141fb3a6baa726a679081be1ec68a9b0cb8b
Mohd Ali <mohdalii1923@gmail.com>
2026-09-20 22:29:10 +0530

    Refactor student information form logic

--- bit_data.py
+++ bit_data.py
@@ -16,8 +16,8 @@ with col2:
     if(st.button("Admin Portal",type="primary")):
         if "admin_verified" not in st.session_state:
             st.session_state.admin_verified=False
-    admin_name=st.text_input("Enter the admin username : ")
-    admin_pass=st.text_input("Enter the admin password : ")
+            admin_name=st.text_input("Enter the admin username : ")
+            admin_pass=st.text_input("Enter the admin password : ")
     if(st.button("Submit",type="primary")):
         if(admin_name==st.secrets["admin_username"] and admin_pass==st.secrets["admin_password"]):
             st.success("Your admin details are verified successfully...!!")
